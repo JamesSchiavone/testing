0e27407a5b0e6abd4dc412b4c1578e8d8e66fe02
JamesSchiavone <triplephat@aol.com>
2021-03-23 09:16:17 -0300

    Initial commit. This one was a pain!

---
+++ The Test.R
@@ -0,0 +1,7 @@
+(print "this file was created within Rstudio")
+
+print ("this file was created within Rstudio")
+
+print("this file was created within Rstudio")
+
+print("and now it lives on Github")
